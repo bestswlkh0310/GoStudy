595cca04b09ba9d0d03f331c4a75be26b54aa48a
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-08-10 17:00:32 +0900

    block & chain & proofOfWork

---
+++ practice_grammer/main/ProofOfWork.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PreBlockHash,
+			pow.Block.Data,
+			IntToHex(pow.Block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+func IntToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	binary.Write(buff, binary.BigEndian, num)
+
+	return buff.Bytes()
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.Block.Data)
+
+	for nonce < math.MaxInt64 {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Print("\n\n")
+
+	return nonce, hash[:]
+}
+
+func (pow *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.Block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+	return isValid
+}
--- practice_grammer/main/main.go
+++ practice_grammer/main/main.go
@@ -1,42 +1,21 @@
 package main
 
 import (
-	"sync"
-	"time"
+	"fmt"
+	"strconv"
 )
 
-type Person struct {
-	Name string
-	Age  int
-}
-
-var mu = sync.Mutex{}
-
-type Movable interface {
-	Run()
-}
-
 func main() {
-	person1 := Person{"h", 1}
-	print(person1.Age)
-	print("\n")
-	defer func() {
-		println("Hello")
-	}()
-	print(Add(1, 3))
-	go func() { PrintSOS() }()
-	go func() { PrintSOS() }()
-	time.Sleep(time.Second * 4)
-}
+	bc := NewBlockChain()
+	bc.AddBlock("asdadad")
+	bc.AddBlock("hello")
 
-func PrintSOS() {
-	//mu.Lock()
-	//defer mu.Unlock()
-	println("Help")
-	time.Sleep(time.Second * 2)
-}
-
-func Add(a int, b int) (result int) {
-	result = a + b
-	return
+	for _, _block := range bc.BlockChain {
+		fmt.Printf("PreHash: %x\n", _block.PreBlockHash)
+		fmt.Printf("Data: %s\n", _block.Data)
+		fmt.Printf("Hash: %x\n", _block.Hash)
+		pow := NewProofOfWork(_block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
 }
